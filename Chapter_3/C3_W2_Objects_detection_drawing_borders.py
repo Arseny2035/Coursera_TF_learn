6e0bcbb5bfa0e077e02260e24ab486990cf72add
Arseny2035 <arsenybandurin@gmail.com>
2022-04-16 11:51:50 +0400

    Add an object detection with bounding box lab

--- Chapter_3/C3_W2_Objects_detection_drawing_borders.py
+++ Chapter_3/C3_W2_Objects_detection_drawing_borders.py
@@ -49,6 +49,7 @@ def display_image(image):
     fig = plt.figure(figsize=(20, 15))
     plt.grid(False)
     plt.imshow(image)
+    plt.show()
 
 
 def download_and_resize_image(url, new_width=256, new_height=256, display=False):
@@ -94,3 +95,202 @@ def download_and_resize_image(url, new_width=256, new_height=256, display=False)
         display_image(pil_image)
 
     return filename
+
+image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg"  #@param
+downloaded_image_path = download_and_resize_image(image_url, 1280, 856, True)
+
+def draw_bounding_box_on_image(image,
+                               ymin,
+                               xmin,
+                               ymax,
+                               xmax,
+                               color,
+                               font,
+                               thickness=4,
+                               display_str_list=()):
+    """
+    Adds a bounding box to an image.
+
+    Args:
+        image -- the image object
+        ymin -- bounding box coordinate
+        xmin -- bounding box coordinate
+        ymax -- bounding box coordinate
+        xmax -- bounding box coordinate
+        color -- color for the bounding box edges
+        font -- font for class label
+        thickness -- edge thickness of the bounding box
+        display_str_list -- class labels for each object detected
+
+
+    Returns:
+        No return.  The function modifies the `image` argument
+                    that gets passed into this function
+
+    """
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+
+    # scale the bounding box coordinates to the height and width of the image
+    (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                  ymin * im_height, ymax * im_height)
+
+    # define the four edges of the detection box
+    draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
+              width=thickness, fill=color)
+
+    # If the total height of the string added to the top of the bounding box exceeds
+    # to the top of the image, stack the string bellow the bounding box instead of above
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+
+    # Each display_str has a top and a bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = top + total_display_str_height
+
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                        (left + text_width, text_bottom)],
+                       fill=color)
+        draw.text((left + margin, text_bottom - text_height - margin),
+                  display_str,
+                  fill="black",
+                  font=font)
+        text_bottom -= text_height - 2 * margin
+
+
+def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
+    """
+    Overlay labeled boxes on an image with formatted scores and label names.
+
+    Args:
+        image -- the image as a numpy array
+        boxes -- list of detection boxes
+        class_names -- list of classes for each detected object
+        scores -- numbers showing the model's confidence in detecting that object
+        max_boxes -- maximum detection boxes to overlay on the image (default is 10)
+        min_score -- minimum score required to display a bounding box
+
+    Returns:
+        image -- the image after detection boxes and classes are overlaid on the original image.
+    """
+    colors = list(ImageColor.colormap.values())
+
+    try:
+        font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
+                                  25)
+    except IOError:
+        print("Font not found, using default font.")
+        font = ImageFont.load_default()
+
+    for i in range(min(boxes.shape[0], max_boxes)):
+
+        # only display detection boxes that have the minimum score or higher
+        if scores[i] >= min_score:
+            ymin, xmin, ymax, xmax = tuple(boxes[i])
+            display_str = "{}: {}%".format(class_names[i].decode("ascii"),
+                                           int(100 * scores[i]))
+            color = colors[hash(class_names[i]) % len(colors)]
+            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+
+            # draw one bounding box and overlay the class labels onto the image
+            draw_bounding_box_on_image(image_pil,
+                                       ymin,
+                                       xmin,
+                                       ymax,
+                                       xmax,
+                                       color,
+                                       font,
+                                       display_str_list=[display_str])
+            np.copyto(image, np.array(image_pil))
+
+    return image
+
+
+def load_img(path):
+    '''
+    Loads a JPEG image and converts it to a tensor.
+
+    Args:
+        path (string) -- path to a locally saved JPEG image
+
+    Returns:
+        (tensor) -- an image tensor
+    '''
+
+    # read the file
+    img = tf.io.read_file(path)
+
+    # convert to a tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+
+    return img
+
+
+def run_detector(detector, path):
+    '''
+    Runs inference on a local file using an object detection model.
+
+    Args:
+        detector (model) -- an object detection model loaded from TF Hub
+        path (string) -- path to an image saved locally
+    '''
+
+    # load an image tensor from a local file path
+    img = load_img(path)
+
+    # add a batch dimension in front of the tensor
+    converted_img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+
+    # run inference using the model
+    start_time = time.time()
+    result = detector(converted_img)
+    end_time = time.time()
+
+    # save the results in a dictionary
+    result = {key: value.numpy() for key, value in result.items()}
+
+    # print results
+    print("Found %d objects." % len(result["detection_scores"]))
+    print("Inference time: ", end_time - start_time)
+
+    # draw predicted boxes over the image
+    image_with_boxes = draw_boxes(
+        img.numpy(), result["detection_boxes"],
+        result["detection_class_entities"], result["detection_scores"])
+
+    # display the image
+    display_image(image_with_boxes)
+
+
+run_detector(detector, downloaded_image_path)
+
+
+image_urls = [
+  # Source: https://commons.wikimedia.org/wiki/File:The_Coleoptera_of_the_British_islands_(Plate_125)_(8592917784).jpg
+  "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_Coleoptera_of_the_British_islands_%28Plate_125%29_%288592917784%29.jpg",
+  # By Américo Toledano, Source: https://commons.wikimedia.org/wiki/File:Biblioteca_Maim%C3%B3nides,_Campus_Universitario_de_Rabanales_007.jpg
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg/1024px-Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg",
+  # Source: https://commons.wikimedia.org/wiki/File:The_smaller_British_birds_(8053836633).jpg
+  "https://upload.wikimedia.org/wikipedia/commons/0/09/The_smaller_British_birds_%288053836633%29.jpg",
+  ]
+
+def detect_img(image_url):
+    start_time = time.time()
+    image_path = download_and_resize_image(image_url, 640, 480)
+    run_detector(detector, image_path)
+    end_time = time.time()
+    print("Inference time:",end_time-start_time)
+
+
+detect_img(image_urls[0])
+
+detect_img(image_urls[1])
+
+detect_img(image_urls[2])
